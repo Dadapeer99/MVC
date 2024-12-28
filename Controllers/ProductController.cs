197a0febe534052e42ff12d9fb8ca59c8507dc33
Dadapeer99 <asdadapeer99@gmail.com>
2024-12-28 18:18:57 +0530

    fh

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleDb.Data;
+using SimpleDb.Models;
+
+namespace SimpleDb.Controllers
+{
+    // Controllers/ProductController.cs
+    public class ProductController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ProductController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var products = _context.Products.ToList();
+            return View(products);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Products.Add(product);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+    }
+}
